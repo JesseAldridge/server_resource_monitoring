547e85b3c2a0268253c1ba7a592a42818d4a020f
Jesse Aldridge <jessealdridge@gmail.com>
2021-07-05 18:11:31 -0500

    first

---
+++ hard_drive.py
@@ -0,0 +1,8 @@
+import shutil
+from datetime import datetime
+
+sleep_secs = 60 * 60 # 1 hour
+while True:
+  total, used, free = shutil.disk_usage("/")
+  print(f"{str(datetime.utcnow()).split('.')[0]} | Used: {used // (2**30)} GiB" )
+  time.sleep(sleep_secs)
